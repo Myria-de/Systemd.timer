617d827c58044f9c4ea2c55b21a9b7fc247991a4
Thorsten Eggeling <Myria-de@users.noreply.github.com>
2024-01-08 17:13:04 +0000

    Update tar-Backup.sh

--- tar-Backup.sh
+++ tar-Backup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 DATE=$(date +%Y-%m-%d-%H%M%S)
 BACKUP_DIR="/mnt/[UUID]/[Backup-Ordner]/"
+# Bei Verwendung mit backup.timer bitte
+# einen Pfad eintragen.
+# Beispielsweise /home/[user]/Dokumente.
 SOURCE="$HOME"
 tar -cvzpf $BACKUP_DIR/backup-home-$DATE.tar.gz $SOURCE
+# Mit rsync
+# rsync -auP $SOURCE/ $BACKUP_DIR
